332ff7c005edc4b9be544a2bf83d30ddb02babf6
marleysidapple <bsiddhartha25@gmail.com>
2017-09-05 11:44:51 +0800

    first push

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+ 
+use Faker\Factory as Faker;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+       /*
+        $faker = Faker::create();
+    	foreach (range(1,50) as $index) {
+	        DB::table('users')->insert([
+	            'name' => $faker->name,
+	            'email' => $faker->email,
+	            'password' => bcrypt('secret'),
+	        ]);
+        }
+        */
+    }
+}
