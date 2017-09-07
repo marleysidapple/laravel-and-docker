a6366263be6660fc534a080d7344da6773c05571
marleysidapple <bsiddhartha25@gmail.com>
2017-09-07 16:08:32 +0800

    added relation

--- app/User.php
+++ app/User.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App;
+use App\Address;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -26,4 +27,11 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+ 
+ public function address()
+ {
+    return $this->hasMany('App\Address');
+ }
+
 }
